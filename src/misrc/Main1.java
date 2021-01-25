5c95682292dbe9d0ddee3acc9c828f7a81fb17db
Sergio Alvaredo <sergio.alvaredo@campusfp.es>
2021-01-25 10:00:47 +0100

    1 SNAPSHOT Main1.java

---
+++ src/misrc/Main1.java
@@ -0,0 +1,12 @@
+package misrc;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        //Declaración del ARRAY//
+        String[] nombre = {"Luis","Miguel","Carlos"};
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);
+        }
+    } 
+}
